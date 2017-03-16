dd166cd7d5e2cde142445d97742e7842ac599a13
kariz711 <kariz711@newschool.edu>
2017-03-16 14:37:23 -0400

    color change

--- screensaver/script2.js
+++ screensaver/script2.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
-  var key = "f6b3bb146ca24b2d9fa24542171503";
-  var url = "https://api.apixu.com/v1/current.json?key=" + key + "&q=NYC";
+// $(document).ready(function(){
+//all js code just draw circles ,svg filter is important code 
 
-$.getJSON(url, function(data){
-	console.log(data["current"]["temp_f"]);
-	// $("#weather").html(data["current"]["temp_f"]);
-	if(data["current"]["temp_f"] <30){
-		$("#weather").html("it's cold af");
-	} else {
-		$("#weather").html("not cold af");
-	}
 
-	if(data["current"]["feelslike_f"] <30){
-		$("#weather").html("feels cold af");
-	} else if(data["current"]["feelslike_f"] >30<60) {
-		$("#weather").html("suck it up");
-	} else {
-		$("#weather").html("fuck yeesssss");
-	}
-	// $("#weather").html(data["current"]["temp_f"]);
-
-});
+/* Need to make this into a function and more modular/generic so it can be used as a plugin. */
+//     var r = 109
+//     var g = 189
+//     var b = 205
+//     var lastScrollTop = 0;
+  
+//     $(window).scroll(function(e){
+  
+//         var maxr = 109;
+//         var maxg = 189;
+//         var maxb = 205;
+  
+//         var st = $(this).scrollTop();
+      
+//         /* Need to change how the calculations work */
+//         if (st > lastScrollTop){
+//             if (r > 0 ) { r -= 20 ; }
+//             if (g > 0 ) { g -= 20 ; }
+//             if (b > 0 ) { b -= 20 ; }
+//         } else {
+//             if (r < maxr ) { r += 20 ; }
+//             if (g < maxg ) { g += 20 ; }
+//             if (b < maxb ) { b += 20 ; }
+//         }
+//         lastScrollTop = st;
+  
+//         $('.element').css({
+//             "elemnet" : "rgba("+r+", "+g+", "+b+", 0.8)"
+//         });
+//     });
+// });
 
 
 });
