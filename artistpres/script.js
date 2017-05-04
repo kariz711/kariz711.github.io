837a2a53a60e1576fa2c243d802f9286cd976ab0
kariz711 <kariz711@newschool.edu>
2017-05-04 15:49:33 -0400

    try 3

---
+++ artistpres/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+$(function(){
+    $('li').okhover();  
+    
+});
+$(function(){
+ $('.ok').okshadow({
+  color: '#588AB6',
+  textShadow: true,
+  xMax: 5,
+  yMax: 5,
+  yOffset: 3,
+  fuzzMin: 1,
+  fuzzMax: 3,
+});
+ });
+
+});
