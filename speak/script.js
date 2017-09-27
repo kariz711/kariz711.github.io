c9b3798862e952822b227f2399c71b5346ce729a
kariz711 <kariz711@newschool.edu>
2017-09-27 13:59:49 -0400

    speak texture

--- speak/script.js
+++ speak/script.js
@@ -6,13 +6,13 @@ var height = movementStrength / $(window).height();
 var width = movementStrength / $(window).width();
 
 
-$("#top-image").mousemove(function(e){
+$(".audioimg").mousemove(function(e){
 	console.log("2");
           var pageX = e.pageX - ($(window).width() / 2);
           var pageY = e.pageY - ($(window).height() / 2);
           var newvalueX = width * pageX * -1 - 25;
           var newvalueY = height * pageY * -1 - 50;
-          $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+          $('audioimg').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
 
 
